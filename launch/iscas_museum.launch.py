d610af71aa10616ab6e11ba10980c24b30910974
nacky823 <youjiyongmu4@gmail.com>
2024-08-19 10:22:35 +0900

    Update launch/iscas_museum.launch.py

--- launch/iscas_museum.launch.py
+++ launch/iscas_museum.launch.py
@@ -2,35 +2,31 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import ExecuteProcess
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+
+    map_path_3d=os.path.join(
+        os.path.expanduser('~'), 'maps', 'iscas_museum', 'iscas_museum_3d.pcd'
+    )
+    map_path_2d=os.path.join(
+        os.path.expanduser('~'), 'maps', 'iscas_museum', 'iscas_museum_2d.yaml'
+    )
+    param_file_name='iscas_museum.param.yaml'
+
+    rviz_config_path=PathJoinSubstitution([
+        FindPackageShare('mugimaru_launcher'), 'config', 'iscas_museum.rviz'
+    ])
 
     use_sim_time=LaunchConfiguration('use_sim_time')
     declare_use_sim_time=DeclareLaunchArgument(
         'use_sim_time',
         default_value='True',
-    )
-
-    rviz_config_file=os.path.join(
-        get_package_share_directory('mugimaru_launcher'),
-        'config', 'iscas_museum.rviz'
-    )
-    rviz2=Node(
-        package='rviz2',
-        executable='rviz2',
-        name='mugimaru_rviz2',
-        namespace='',
-        arguments=['-d', rviz_config_file],
-        parameters=[{'use_sim_time': use_sim_time}],
-        output='screen',
-        prefix='xterm -e',
     )
 
     define_world=Node(
@@ -43,6 +39,7 @@ def generate_launch_description():
             '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
             '--frame-id', 'world', '--child-frame-id', 'map',
         ],
+        parameters=[{'use_sim_time': use_sim_time}],
         output='screen',
     )
     define_livox_frame=Node(
@@ -55,48 +52,69 @@ def generate_launch_description():
             '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
             '--frame-id', 'base_link', '--child-frame-id', 'livox_frame',
         ],
+        parameters=[{'use_sim_time': use_sim_time}],
         output='screen',
     )
 
+    rviz2=Node(
+        package='rviz2',
+        executable='rviz2',
+        name='mugimaru_rviz2',
+        namespace='',
+        arguments=['-d', rviz_config_path],
+        parameters=[{'use_sim_time': use_sim_time}],
+        output='screen',
+        prefix='xterm -e',
+    )
+
     exec_gazebo=ExecuteProcess(
-        cmd=[ 'ros2', 'launch', 'raspicat_gazebo', 'raspicat_with_iscas_museum.launch.py', 'rviz:=false' ],
+        cmd=['ros2', 'launch', 'raspicat_gazebo', 'raspicat_with_iscas_museum.launch.py', 'rviz:=false'],
         output='screen',
     )
     exec_twist=ExecuteProcess(
-        cmd=[ 'ros2', 'launch', 'ros2_odometry_twist_converter', 'mugimaru_sim.launch.py' ],
+        cmd=['ros2', 'launch', 'ros2_odometry_twist_converter', 'mugimaru.launch.py',
+            f'use_sim_time:={use_sim_time}'],
         output='screen',
     )
     exec_gyro=ExecuteProcess(
-        cmd=[ 'ros2', 'launch', 'gyro_odometer', 'sim_iscas.launch.xml' ],
+        cmd=['ros2', 'launch', 'gyro_odometer', 'mugimaru.launch.py',
+            f'use_sim_time:={use_sim_time}'],
         output='screen',
     )
     exec_map=ExecuteProcess(
-        cmd=[ 'ros2', 'launch', 'map_loader', 'sim_iscas.launch.py' ],
+        cmd=['ros2', 'launch', 'map_loader', 'mugimaru.launch.py',
+            f'use_sim_time:={use_sim_time}', f'param_file_name:={param_file_name}',
+            f'map_path:={map_path_3d}'],
         output='screen',
     )
     exec_points=ExecuteProcess(
-        cmd=[ 'ros2', 'launch', 'pointcloud_preprocessor', 'sim_iscas.launch.py' ],
+        cmd=['ros2', 'launch', 'pointcloud_preprocessor', 'mugimaru.launch.py',
+            f'use_sim_time:={use_sim_time}', f'param_file_name:={param_file_name}'],
         output='screen',
     )
     exec_ndt=ExecuteProcess(
-        cmd=[ 'ros2', 'launch', 'ndt_scan_matcher', 'sim_iscas.launch.py' ],
+        cmd=['ros2', 'launch', 'ndt_scan_matcher', 'mugimaru.launch.py',
+            f'use_sim_time:={use_sim_time}', f'param_file_name:={param_file_name}'],
         output='screen',
     )
     exec_ekf=ExecuteProcess(
-        cmd=[ 'ros2', 'launch', 'ekf_localizer', 'sim_iscas.launch.xml' ],
+        cmd=['ros2', 'launch', 'ekf_localizer', 'mugimaru.launch.py',
+            f'use_sim_time:={use_sim_time}', f'param_file_name:={param_file_name}'],
         output='screen',
     )
     exec_nav2=ExecuteProcess(
-        cmd=[ 'ros2', 'launch', 'mugimaru_navigation2', 'iscas_museum.launch.py' ],
+        cmd=['ros2', 'launch', 'mugimaru_navigation2', 'mugimaru.launch.py',
+            f'use_sim_time:={use_sim_time}', f'param_file_name:={param_file_name}',
+            f'map_path:={map_path_2d}'],
         output='screen',
     )
 
     ld=LaunchDescription()
 
     ld.add_action(declare_use_sim_time)
-    ld.add_action(rviz2)
     ld.add_action(define_world)
     ld.add_action(define_livox_frame)
+    ld.add_action(rviz2)
 
     ld.add_action(exec_gazebo)
     ld.add_action(exec_twist)
